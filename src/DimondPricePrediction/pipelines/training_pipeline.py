cd4f98d1f87c58a7cfdc90179e594ad364b3c018
Umme Ayesha Siddika <asiddikaeee@gmail.com>
2025-07-12 11:24:48 +0900

    updated

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -1,6 +1,8 @@
 from src.DimondPricePrediction.components.data_ingestion import DataIngestion
 from src.DimondPricePrediction.components.data_transformation import DataTransformation
 from src.DimondPricePrediction.components.model_trainer import ModelTrainer
+from src.DimondPricePrediction.components.model_evaluation import ModelEvaluation
+
 
 
 import os
